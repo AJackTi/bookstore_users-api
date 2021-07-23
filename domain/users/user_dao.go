bd4bc67acadab8d7c723e75b5677a09cdb9f9dc0
Trong <dtrong97vn@gmail.com>
2021-07-23 22:33:21 +0700

    004 How to handle MySQL errors

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -1,37 +1,89 @@
 package users
 
 import (
-	"fmt"
+	"github.com/AjackTi/bookstore_users-api/datasource/mysql/users_db"
+	"github.com/AjackTi/bookstore_users-api/utils/date_utils"
 	"github.com/AjackTi/bookstore_users-api/utils/errors"
+	"github.com/AjackTi/bookstore_users-api/utils/mysql_utils"
 )
 
-var (
-	usersDB = make(map[int64]*User)
+const (
+	indexUniqueEmail = "email_UNIQUE"
+	errorNoRows      = "no rows in result set"
+	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
+	queryGetUser     = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?"
 )
 
-func (user User) Get() *errors.RestErr {
-	result := usersDB[user.Id]
-	if result == nil {
-		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+//var (
+//	usersDB = make(map[int64]*User)
+//)
+
+func (user *User) Get() *errors.RestErr {
+	//if err := users_db.Client.Ping(); err != nil {
+	//	panic(err)
+	//}
+
+	//result := usersDB[user.Id]
+	//if result == nil {
+	//	return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+	//}
+	//
+	//user.Id = result.Id
+	//user.FirstName = result.FirstName
+	//user.LastName = result.LastName
+	//user.Email = result.Email
+	//user.DateCreated = result.DateCreated
+
+	stmt, err := users_db.Client.Prepare(queryGetUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
 
-	user.Id = result.Id
-	user.FirstName = result.FirstName
-	user.LastName = result.LastName
-	user.Email = result.Email
-	user.DateCreated = result.DateCreated
+	defer stmt.Close()
+
+	result := stmt.QueryRow(user.Id)
+
+	if getErr := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); getErr != nil {
+		return mysql_utils.ParseError(getErr)
+	}
+
 	return nil
 }
 
 func (user *User) Save() *errors.RestErr {
-	current := usersDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
 
-	usersDB[user.Id] = user
+	defer stmt.Close()
+
+	user.DateCreated = date_utils.GetNowString()
+
+	insertResult, saveErr := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if saveErr != nil {
+		return mysql_utils.ParseError(saveErr)
+	}
+
+	//result, err := users_db.Client.Exec(queryInsertUser, user.FirstName, user.LastName, user.Email, user.DateCreated)
+
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return mysql_utils.ParseError(err)
+	}
+
+	//current := usersDB[user.Id]
+	//if current != nil {
+	//	if current.Email == user.Email {
+	//		return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
+	//	}
+	//	return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	//}
+	//
+	//user.DateCreated = date_utils.GetNowString()
+	//usersDB[user.Id] = user
+
+	user.Id = userId
+
 	return nil
 }
